1ec97fdd00ac1c46ee4d19340c1a6193af40f1a2
chrisspencer1013 <chrisspencer1013@gmail.com>
2017-06-27 11:16:39 -0400

    Update itsybitsy.py

--- itsybitsy.py
+++ itsybitsy.py
@@ -1,3 +1,28 @@
 import os
+from datetime import datetime
 
 # will contain stuff for the actual crawler
+
+
+def initialize(project_dir, url):
+    if not os.path.exists(project_dir):
+        print("Project Directory created at: " + project_dir)
+        os.makedirs(project_dir)
+    queue = project_dir + "/queue.txt"
+    crawled = project_dir + "/all_links.txt"
+    if not os.path.isfile(queue):
+        write_me(path = queue, data = str(datetime.now()) + " | " + url, isNewFile = 1, isOverWrite = 1) #TODO: Format pretty
+    if not os.path.isfile(crawled):
+        write_me(path = crawled, data = "", isNewFile = 1, isOverWrite = 1)
+
+def write_me(path, data, isNewFile, isOverWrite):
+    if isOverWrite:
+        file = open(path, 'w')
+        if isNewfile:
+            file.write(data)
+        file.close()
+    else:
+        with open(path, 'a') as file:
+            file.write(data + '\n')
+
+        
